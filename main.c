14a102ee7f449b6663894962b4e4b1e9380c228b
FeelsCoderMan <onurlul@hotmail.com>
2023-09-07 22:55:04 +0300

    Added player state, hovering on restart button

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include "include/raylib.h"
 
@@ -35,27 +36,33 @@ typedef enum {
     CONTINUE
 } match_state;
 
+typedef enum {
+    PLAYER_USER,
+    PLAYER_AI
+} player_state;
+
 void draw_tiles(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], Vector2 *hovered_tile_position);
 Vector2 get_tile_from_pos(Vector2 *mouse_position);
-void update_tiles_on_mouse_position(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], Vector2 *hovered_tile_position, int *current_player);
+void update_tiles_on_mouse_position(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], Vector2 *hovered_tile_position, player_state *current_player);
 match_state check_win_condition(int tiles[NUM_OF_ROWS][NUM_OF_ROWS]);
 match_state check_tie_condition(int tiles[NUM_OF_ROWS][NUM_OF_ROWS]);
-void reset_game(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], match_state *current_match_state);
+void reset_game(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], match_state *current_match_state, player_state *current_player);
 void render_outcome_ui(match_state *current_match_state);
 
 int main (void) {
     int tiles[NUM_OF_ROWS][NUM_OF_ROWS] = {EMPTY};
-    int current_player = 0;
+    player_state current_player = PLAYER_USER;
     bool exit = false;
     Vector2 hovered_tile_position = {-1, -1};
     match_state current_match_state;
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Simple Tic Tac Toe Game");
-    // TODO: Add !WindowShouldClose to game loop
-    // TODO: Add AI movement
+
+    SetTargetFPS(60);
     // TODO: Update makefile with pkg config of raylib, remove lib static file
     // TODO: (Optional) Trigger a sound when user makes a move
-    while (!exit) {
+    // TODO: Add AI movement
+    while (WindowShouldClose() || !exit) {
         if (IsKeyPressed(KEY_Y)) {
             exit = true;
         }
@@ -66,7 +73,7 @@ int main (void) {
 
         if (current_match_state != CONTINUE) {
             render_outcome_ui(&current_match_state);
-            reset_game(tiles, &current_match_state);
+            reset_game(tiles, &current_match_state, &current_player);
         } else {
             update_tiles_on_mouse_position(tiles, &hovered_tile_position, &current_player);
             ClearBackground(BACKGROUND_COLOR);
@@ -94,11 +101,20 @@ void render_outcome_ui(match_state *current_match_state) {
         DrawText("No winner", SCREEN_WIDTH * 2 / 7, SCREEN_HEIGHT / 3, 75, OUTCOME_COLOR);
     }
 
-    DrawRectangleLines(RESTART_BUTTON_START_X, RESTART_BUTTON_START_Y, RESTART_BUTTON_WIDTH, RESTART_BUTTON_HEIGHT, WHITE);
-    DrawText("Restart", SCREEN_WIDTH * 3 / 8, SCREEN_HEIGHT / 2, 50, WHITE);
+    Vector2 mouse_position = GetMousePosition();
+
+    if (mouse_position.x > RESTART_BUTTON_START_X && mouse_position.x < RESTART_BUTTON_START_X + RESTART_BUTTON_WIDTH &&
+            mouse_position.y > RESTART_BUTTON_START_Y && mouse_position.y < RESTART_BUTTON_START_Y + RESTART_BUTTON_HEIGHT) {
+        DrawRectangle(RESTART_BUTTON_START_X, RESTART_BUTTON_START_Y, RESTART_BUTTON_WIDTH, RESTART_BUTTON_HEIGHT, WHITE);
+        DrawText("Restart", SCREEN_WIDTH * 3 / 8, SCREEN_HEIGHT / 2, 50, BLACK);
+    } else {
+        DrawRectangleLines(RESTART_BUTTON_START_X, RESTART_BUTTON_START_Y, RESTART_BUTTON_WIDTH, RESTART_BUTTON_HEIGHT, WHITE);
+        DrawText("Restart", SCREEN_WIDTH * 3 / 8, SCREEN_HEIGHT / 2, 50, WHITE);
+    }
+
 }
 
-void reset_game(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], match_state *current_match_state) {
+void reset_game(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], match_state *current_match_state, player_state *current_player) {
     if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
         Vector2 mouse_position = GetMousePosition();
 
@@ -106,6 +122,7 @@ void reset_game(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], match_state *current_match_
                 mouse_position.y > RESTART_BUTTON_START_Y && mouse_position.y < RESTART_BUTTON_START_Y + RESTART_BUTTON_HEIGHT) {
             memset(tiles, EMPTY, sizeof(tiles[0][0]) * NUM_OF_ROWS * NUM_OF_ROWS);
             *current_match_state = CONTINUE;
+            *current_player = PLAYER_USER;
         }
     }
 }
@@ -119,7 +136,7 @@ match_state check_diagonal_line(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], char* direc
 
         if (strcmp(direction, "LEFT") ==  0) {
             tile_status = tiles[i][i];
-        } else if (strcmp(direction, "RIGHT")) {
+        } else if (strcmp(direction, "RIGHT") == 0) {
             tile_status = tiles[i][NUM_OF_ROWS - 1 - i];
         }
             
@@ -221,7 +238,7 @@ match_state check_tie_condition(int tiles[NUM_OF_ROWS][NUM_OF_ROWS]) {
     return count_pressed_tiles == NUM_OF_ROWS * NUM_OF_ROWS ? TIE : CONTINUE;
 }
 
-void update_tiles_on_mouse_position(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], Vector2 *hovered_tile_position, int *current_player) {
+void update_tiles_on_mouse_position(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], Vector2 *hovered_tile_position, player_state *current_player) {
     Vector2 mouse_position = GetMousePosition();
     Vector2 tile_position = get_tile_from_pos(&mouse_position);
 
@@ -230,15 +247,14 @@ void update_tiles_on_mouse_position(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], Vector2
             tile_state pressed_tile_state = tiles[(int)tile_position.x][(int)tile_position.y];
             
             if (pressed_tile_state == EMPTY) {
-                tiles[(int)tile_position.x][(int)tile_position.y] = *current_player == 0 ? USER : AI;
-                *current_player = *current_player ? 0 : 1;
+                tiles[(int)tile_position.x][(int)tile_position.y] = *current_player == PLAYER_USER ? USER : AI;
+                *current_player = *current_player == PLAYER_USER ? PLAYER_AI : PLAYER_USER;
             }
         } else {
             hovered_tile_position->x = tile_position.x;
             hovered_tile_position->y = tile_position.y;
         }
     }
-
 }
 
 void draw_tiles(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], Vector2 *hovered_tile_position) {
@@ -264,7 +280,6 @@ void draw_tiles(int tiles[NUM_OF_ROWS][NUM_OF_ROWS], Vector2 *hovered_tile_posit
                         Rectangle left_rect = {starting_point_x + TILE_WIDTH / 3, starting_point_y + TILE_HEIGHT / 8, TILE_WIDTH * 3 / 4, TILE_HEIGHT / 4};
                         Rectangle right_rect = {starting_point_x + TILE_WIDTH * 2 / 3, starting_point_y + TILE_HEIGHT / 8, TILE_WIDTH / 4, TILE_HEIGHT * 3 / 4};
                         Vector2 origin = {0, 0};
-                        
                         DrawRectangle(starting_point_x, starting_point_y, TILE_WIDTH, TILE_HEIGHT, DEFAULT_TILE_COLOR);
                         DrawRectanglePro(left_rect, origin, 45.f, CROSS_COLOR);
                         DrawRectanglePro(right_rect, origin, 45.f, CROSS_COLOR);
